d1a7d4e12a0b37d73e38f92eeeba4af4fec491ab
Abdulraheem <raimitaiwo45@gmail.com>
2022-09-27 13:59:43 +0100

    new commit

--- script.js
+++ script.js
@@ -31,3 +31,40 @@ clearDiv.addEventListener('click', (e)=>{
 })
 
 
+/*
+let randomContainer = document.querySelector(".randomContainer");
+// let input = document.getElementById('input');
+let randomValue = Math.trunc(Math.random() * 50) + 51;
+// console.log(randomValue);
+let randomClick = document.querySelector("randomClick")
+
+randomClick.addEventListener('click', (e)=>{
+  e.preventDefault();
+
+  for (i = 1; i <= randomValue; i++) {
+    // let values100 = document.createElement("h1");
+    // document.getElementById("divContainer").appendChild(values100);
+  
+    if (i % 2 == 0) {
+      values100.textContent = i;
+      // values100.className = 'even';
+      values100.classList.add('even');
+  
+    } else {
+      values100.textContent = i;
+      values100.className = 'odd';
+      // values100.style.backgroundColor = "yellow";
+    }
+  };
+  input.value = '';
+  
+
+})
+
+// let clearDiv = document.querySelector('.clear-div');
+clearDiv.addEventListener('click', (e)=>{
+  div.textContent = '';
+})
+
+// let randomValue = document.createElement('div');
+*/
